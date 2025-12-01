f03cf85ab594ef4872a8d13c9e539e1dda60a1db
pedro558 <pedroafonso109@gmail.com>
2025-12-01 13:51:00 -0300

    :sparkles: feat: add /hello endpoint to test inserting and retrieving data

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import fastify from "fastify";
+import { randomUUID } from 'node:crypto';
 import { knex } from "./database";
 
 const app = fastify();
 
-app.get("/health", async() => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+app.get("/hello", async() => {
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000
+  //   })
+  //   .returning('*');
+  const transactions = await knex('transactions').select('*');
+
+  return transactions
   
 });
 
