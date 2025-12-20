1126ee0974a44fbbcca2bf04a49877988bafc35c
pedro558 <pedroafonso109@gmail.com>
2025-12-19 21:36:02 -0300

    :recycle: refactor: implement fastify plugin routes

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,13 @@ import fastify from "fastify";
 import { randomUUID } from 'node:crypto';
 import { knex } from "./database";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
 
-app.get("/hello", async() => {
-  // const transaction = await knex('transactions')
-  //   .insert({
-  //     id: randomUUID(),
-  //     title: 'Transação de teste',
-  //     amount: 1000
-  //   })
-  //   .returning('*');
-  const transactions = await knex('transactions').select('*');
-
-  return transactions
-  
-});
+app.register(transactionsRoutes, {
+  prefix: 'transactions'
+})
 
 app
   .listen({
