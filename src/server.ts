fabb55105e5fedbc0dfc32ee58c953739947df0a
pedro558 <pedroafonso109@gmail.com>
2025-12-20 11:25:18 -0300

    :sparkles: feat: add fastify/cookie function inside server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import fastify from "fastify";
-import { randomUUID } from 'node:crypto';
-import { knex } from "./database";
 import { env } from "./env";
+import cookie from "@fastify/cookie";
 import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
 
+app.register(cookie)
 app.register(transactionsRoutes, {
   prefix: 'transactions'
 })
